c74cb38047677d46a49191df8a59970a2101f589
mkiuzzal01 <mkiuzzal07@gmail.com>
2025-01-15 14:33:57 +0600

    advertisement route created

---
+++ src/app/(main)/dashboard/add-advertisement/__components/AddAdvertisementForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddAdvertisementForm = () => {
+  return (
+    <div>
+      <h1>This is advertisement form</h1>
+    </div>
+  );
+};
+
+export default AddAdvertisementForm;
--- src/app/(main)/dashboard/add-advertisement/page.tsx
+++ src/app/(main)/dashboard/add-advertisement/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Page = () => {
   return (
     <div>
-      <h1>This is Add Page</h1>
+      <h1>This is Advertisement</h1>
     </div>
   );
 };
