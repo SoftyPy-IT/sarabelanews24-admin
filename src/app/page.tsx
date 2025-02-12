cdacf75e72a4ef87b61e9c2468304899aae87af3
SoftyPy-IT <softypy@gmail.com>
2025-02-12 11:38:31 +0600

    add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 "use client";
 
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
@@ -30,7 +31,7 @@ type FormData = {
 
 
 const ParticleBackground = () => {
-  const backgroundTexts = ["সত্যের সন্ধানে সব সময়", "daytimes24"];
+  const backgroundTexts = useMemo(() => ["সত্যের সন্ধানে সব সময়", "daytimes24"], []);
   const [textInstances, setTextInstances] = useState<
     {
       text: string;
@@ -45,6 +46,9 @@ const ParticleBackground = () => {
     animationDelay: `${Math.random() * 3}s`,
   });
 
+
+
+
   const generateTextInstances = useCallback((count: number) => {
     const instances: {
       text: string;
@@ -52,7 +56,7 @@ const ParticleBackground = () => {
       position: { top: string; left: string; animationDelay: string };
     }[] = [];
     for (let i = 0; i < count; i++) {
-      backgroundTexts.forEach((text) => {
+      backgroundTexts.forEach((text:any) => {
         instances.push({
           text,
           id: `${text}-${i}`,
@@ -61,12 +65,15 @@ const ParticleBackground = () => {
       });
     }
     return instances;
-  }, []); // No dependencies since `backgroundTexts` is a constant
+  }, [backgroundTexts]);
   
   useEffect(() => {
     const instances = generateTextInstances(10);
     setTextInstances(instances);
   }, [generateTextInstances]);
+
+
+  
 
   // const generateTextInstances = (count: number) => {
   //   const instances: {
