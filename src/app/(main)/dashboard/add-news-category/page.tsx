16fca0e46097ab9a962aafb9f9cac9d902ed9712
mkiuzzal01 <mkiuzzal07@gmail.com>
2025-07-26 13:07:17 +0600

    add readme file

---
+++ src/app/(main)/dashboard/add-news-category/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddNewsCategory from "./__components/AddNewsCategory";
+
+const Page = () => {
+  return (
+    <div>
+      <AddNewsCategory />
+    </div>
+  );
+};
+
+export default Page;
